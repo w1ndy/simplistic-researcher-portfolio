93c4c0bb3f7df57345f28f524bb19473641eea51
Di Weng <diweng@microsoft.com>
2023-11-06 14:30:33 +0800

    fix publication list scrolling

--- src/pages/Publications.tsx
+++ src/pages/Publications.tsx
@@ -4,20 +4,22 @@ import { PaperEntry } from '../components/PaperEntry'
 
 export default function Publications() {
   return (
-    <div class="max-w-4xl pr-3 overflow-y-scroll pb-4">
-      <div class="italic mb-4  text-gray-500">
-        *: corresponding authors are marked with asterisk.
+    <div class="overflow-y-scroll">
+      <div class="max-w-4xl pr-3 pb-4">
+        <div class="italic mb-4  text-gray-500">
+          *: corresponding authors are marked with asterisk.
+        </div>
+        <For each={PublicationEntries}>
+          {(rp) => (
+            <div class="mb-4">
+              <div class="text-gray-500 font-bold">{rp.year}</div>
+              <For each={rp.papers}>
+                {(paper) => <PaperEntry paper={paper} />}
+              </For>
+            </div>
+          )}
+        </For>
       </div>
-      <For each={PublicationEntries}>
-        {(rp) => (
-          <div class="mb-4">
-            <div class="text-gray-500 font-bold">{rp.year}</div>
-            <For each={rp.papers}>
-              {(paper) => <PaperEntry paper={paper} />}
-            </For>
-          </div>
-        )}
-      </For>
     </div>
   )
 }
