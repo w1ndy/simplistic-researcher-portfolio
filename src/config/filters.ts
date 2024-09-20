ca18458cfd2c76587434648f6e57ab34c6c19836
Di Weng <diweng@microsoft.com>
2024-09-20 12:08:07 +0800

    add filters

---
+++ src/config/filters.ts
@@ -0,0 +1,25 @@
+import { Paper } from '../types/paper'
+
+export const Filters: Record<
+  string,
+  { name: string; matcher: (paper: Paper) => boolean }
+> = {
+  authorship: {
+    name: 'First/corresponding-authored',
+    matcher: (paper: Paper) =>
+      paper.corresponding_authors?.includes('Di Weng') ||
+      paper.authors[0] == 'Di Weng',
+  },
+  tvcg: {
+    name: 'TVCG',
+    matcher: (paper: Paper) => paper.venue.includes('TVCG'),
+  },
+  chi: {
+    name: 'CHI',
+    matcher: (paper: Paper) => paper.venue.includes('CHI'),
+  },
+  kdd: {
+    name: 'KDD',
+    matcher: (paper: Paper) => paper.venue.includes('KDD'),
+  },
+}
--- src/pages/Publications.tsx
+++ src/pages/Publications.tsx
@@ -1,15 +1,73 @@
-import { For } from 'solid-js'
-import PublicationEntries from '../config/publications'
+import { createMemo, For } from 'solid-js'
 import { PaperEntry } from '../components/PaperEntry'
+import { createStore } from 'solid-js/store'
+import _ from 'lodash'
+import Fa from 'solid-fa'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+import PublicationEntries from '../config/publications'
+import { Filters } from '../config/filters'
 
 export default function Publications() {
+  const [filterActive, setFilterActive] = createStore(
+    _.mapValues(Filters, () => false)
+  )
+
+  const filteredPublications = createMemo(() =>
+    PublicationEntries.map((year) => ({
+      ...year,
+      papers: year.papers.filter((paper) => {
+        for (const filter in Filters) {
+          if (filterActive[filter] && !Filters[filter].matcher(paper)) {
+            return false
+          }
+        }
+        return true
+      }),
+    }))
+  )
+
+  const filterCounts = createMemo(() => {
+    const counts = _.mapValues(Filters, () => 0)
+    filteredPublications().forEach((year) => {
+      year.papers.forEach((paper) => {
+        for (const filter in Filters) {
+          if (Filters[filter].matcher(paper)) {
+            counts[filter]++
+          }
+        }
+      })
+    })
+    return counts
+  })
+
   return (
     <div class="overflow-y-scroll">
       <div class="max-w-4xl pr-3 pb-4">
-        <div class="italic mb-4  text-gray-500">
-          *: corresponding authors are marked with asterisk.
+        <div>
+          <span class="font-bold">
+            <Fa
+              icon={faMagnifyingGlass}
+              class="inline-block scale-75"
+            ></Fa>{' '}
+            filter by:{' '}
+          </span>
+          <For each={Object.entries(Filters)}>
+            {([key, { name }]) => (
+              <span
+                class="ml-2 cursor-pointer select-none"
+                classList={{ 'text-teal-700': filterActive[key] }}
+                onClick={() => setFilterActive(key, !filterActive[key])}
+              >
+                [{name} ({filterCounts()[key]})]
+              </span>
+            )}
+          </For>
         </div>
-        <For each={PublicationEntries}>
+        <div class="italic mb-4  text-gray-500">
+          *: corresponding-authored papers are marked with asterisk.
+        </div>
+        <For each={filteredPublications()}>
           {(rp) => (
             <div class="mb-6">
               <div class="text-gray-500 font-bold">{rp.year}</div>
